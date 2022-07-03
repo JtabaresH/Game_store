21df2d12d29c0f7bf8429241627df91d388f6af7
JtabaresH <71291097+JtabaresH@users.noreply.github.com>
2022-07-03 06:47:23 -0500

    update

---
+++ routes/users.routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+
+// Controllers
+const { createUser, loginUser, getAllActiveUsers, updateUser, disableUser } = require('../controllers/users.controller')
+const { userExists } = require('../middlewares/users.middleware')
+const { createUserValidators } = require('../middlewares/validators.middleware')
+
+const usersRouter = express.Router()
+
+usersRouter.post('/singup', createUserValidators, createUser)
+usersRouter.post('/login', loginUser)
+usersRouter.get('/', getAllActiveUsers)
+usersRouter.patch('/:id', userExists, updateUser)
+usersRouter.delete('/:id', userExists, disableUser)
+
+module.exports = { usersRouter }
